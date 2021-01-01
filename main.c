58e1e80edd0548d6d3d356851dfa25ffc181ed18
vuviethung1998 <vuviethung.98.hust@gmail.com>
2021-01-01 22:59:57 +0700

    update

--- main.c
+++ main.c
@@ -4,32 +4,32 @@ int main(){
   do{
     printf("----------------------\n");
     printf("MENU\n");
-    printf("1. \n");
-    printf("2. \n");
-    printf("3. \n");
-    printf("4. \n");
-    printf("5. \n");
-    printf("6. \n");
+    printf("1. Simulate connection\n");
+    printf("2. Show all forwarding table\n");
+    printf("3. Show all link state table\n");
+    printf("4. Modify network\n");
+//    printf("5. \n");
+//    printf("6. \n");
     do{
       printf("Enter: ");
       scanf(" %d", &op);
-    }while(op> 6 || op<1);
+    }while(op> 4 || op<1);
 
     switch(op){
     case 1:
-
+      printf("Choose 1");
       break;
 
     case  2:
-
+      printf("Choose 2");
       break;
 
     case  3:
-
+      printf("Choose 3");
       break;
 
     case  4:
-
+      printf("Choose 4");
       break;
 
 
